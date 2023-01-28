c5d7dd4360a5fd3eec17264ead40785bfbc396b9
heatherbaier <hmbaier@wm.edu>
2023-01-28 16:18:41 -0700

    updates

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__)
 
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
 
 
-@app.route("/<string:name>/")
-def say_hello(name):
-    return f"Hello {name}!"
-
-
-if __name__ == "__main__":
-    app.run()
+# http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    return render_template('index.html', msg='')
